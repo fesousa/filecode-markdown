72e9a545a651ae006db4d152719f8eba3d6225d1
Fernando Sousa <fernandosousa.ti@gmail.com>
2021-10-31 11:56:48 -0300

    first commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 git config --global user.email "fernandosousa.ti@gmail.com"
 git config --global user.name "Fernando Sousa|Actions"
-git clone https://github.com/${INPUT_OWNER}/${INPUT_REPO}.git       
+git clone https://${INPUT_OWNER}@${INPUT_ACCESS_TOKE}github.com/${INPUT_OWNER}/${INPUT_REPO}.git       
 python3 .github/workflows/readme_code.py ${INPUT_REPO}
 cd ${INPUT_REPO}
 git add README.md
