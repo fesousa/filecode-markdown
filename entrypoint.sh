79e10a01f92f0d781230642c80f8f40bcacf6006
Fernando Sousa <fernandosousa.ti@gmail.com>
2021-10-31 12:27:15 -0300

    first commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,6 @@
+git config --global user.email "${INPUT_EMAIL}"
+git config --global user.name "${INPUT_USER}|Actions"
+pwd
 python3 /readme_code.py
 git add README.md
 git commit -m 'alteração readme'
