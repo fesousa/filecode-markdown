064d529ef1c4260a9e0917eaf71920177a1225f8
Fernando Sousa <fernandosousa.ti@gmail.com>
2021-10-31 12:14:50 -0300

    first commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 git config --global user.email "fernandosousa.ti@gmail.com"
 git config --global user.name "Fernando Sousa|Actions"
 cd ..
-cd ..
 ls
 python3 /readme_code.py
 git add README.md
--- readme_code.py
+++ readme_code.py
@@ -1,6 +1,6 @@
 import re
 import sys
-with open(f'/home/README.template.md', 'r') as readme:
+with open(f'/home/home/README.template.md', 'r') as readme:
     content = readme.read()
     subs = re.findall(r'```.*?\n(\$\{.*?\})\n```', content)
     for s in subs:
@@ -9,5 +9,5 @@ with open(f'/home/README.template.md', 'r') as readme:
         with open(file) as f:
             content = content.replace(s, f.read())
     
-with open(f'/home/README.md', 'w') as readme:    
+with open(f'/home/home/README.md', 'w') as readme:    
     readme.write(content)
