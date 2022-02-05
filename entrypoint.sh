7ee958c80bdcf911ca68b3f03e2aaa6fe7e99680
Fernando Sousa <fernandosousa.ti@gmail.com>
2022-02-05 16:32:06 -0300

    inclusão de data no arquivo readme

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,6 @@ git config --global user.name "${INPUT_USER}|Actions"
 pwd
 python3 /readme_code.py
 git add README.md
-git commit -m 'alteração readme'
+echo `date` >> README.md
+git commit -m (git log -1 --pretty=%B)
 git push origin master
