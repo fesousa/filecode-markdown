5a7aab2b233548f389833d202ff759a12a1bbdc7
Fernando Sousa <fernandosousa.ti@gmail.com>
2022-03-12 20:51:38 -0300

    melhorias diversas

--- readme_code.py
+++ readme_code.py
@@ -2,10 +2,9 @@ import re
 from datetime import datetime
 with open(f'/github/workspace/template/README.template.md', 'r') as readme:
     content = readme.read()
-    subs = re.findall(r'\s{0,12}```.*?\n\s{0,12}(\$\{.*?\})\n\s{0,12}```', content)
+    subs = re.findall(r'```.*?\n(\$\{.*?\})\n```', content)
     for s in subs:
         file = s[2:-1]
-        print(file)
         with open(file) as f:
             content = content.replace(s, f.read())
     content = content.replace("{{update}}", f"\nLast update: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
